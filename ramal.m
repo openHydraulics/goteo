07e7ff0e55d6dfe482047d8d039cd519268ab82f
Raúl Sánchez <raul.sanchez@upm.es>
2020-02-10 00:07:04 +0100

    Cálculo de la distribución presión mediante iteracciones

--- ramal.m
+++ ramal.m
@@ -1,3 +1,5 @@
+clear;clc
+
 %Cálculo de la distribución de caudal en un ramal de riego por goteo
 h0=10;n=100;k=1.25;x=0.54;CVm=0.05;le=0.5;s=0.5;D=13.6;I0=0.05;
 
@@ -10,11 +12,21 @@ zR=-I0.*xR;
 %Variación manufactura
 varManuf=1+CVm.*randn(1,n);
 
-h=h0.*vectorUnos-zR;
-q=(k.*h.^x).*varManuf;
-h=h0.*vectorUnos-zR-(0.465.*(q*tril(ones(n),0)).^1.75.*D.^-4.75.*(le+s))*transpose(matrizAcum);
+h=h0.*vectorUnos;
+hant=0.*vectorUnos;
+
+i=0;
+tic;
+while sum(abs(h-hant))>1e-6;
+  hant=h;
+  q=(k.*h.^x).*varManuf;
+  ++i;
+  h=h0.*vectorUnos-zR-(0.465.*(q*tril(ones(n),0)).^1.75.*D.^-4.75.*(le+s))*transpose(matrizAcum);
+end
+toc
 
 %Resultados
+i
 h0
 CVq=std(q)/mean(q)
 CVqh=std(k.*h.^x)/mean(k.*h.^x)
