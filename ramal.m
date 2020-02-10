4a54d3bd1343a280974a59cde55b767c5757cf3c
Raúl Sánchez <raul.sanchez@upm.es>
2020-02-10 17:05:25 +0100

    Cálculo de la unidad como un conjunto de ramales dispuestos a lo largo de una tubería portarramales

--- ramal.m
+++ ramal.m
@@ -1,35 +1,19 @@
-clear;clc
+ %Cálculo de la distribución de presión (y caudal) en un ramal de riego por goteo
+function [h,q]=ramal(h0,n,k,x,CVm,le,s,D,I0,varManuf)
 
-%Cálculo de la distribución de caudal en un ramal de riego por goteo
-h0=10;n=100;k=1.25;x=0.54;CVm=0.05;le=0.5;s=0.5;D=13.6;I0=0.05;
+  vectorUnos=ones(n,1);
+  matrizAcum=triu(ones(n),0);
 
-vectorUnos=ones(1,n);
-matrizAcum=tril(ones(n),0);
+  xR=transpose(matrizAcum)*(s.*vectorUnos);
+  zR=-I0.*xR;
 
-xR=(s.*vectorUnos)*transpose(matrizAcum);
-zR=-I0.*xR;
+  h=h0.*vectorUnos;
+  hant=0.*vectorUnos;
 
-%Variación manufactura
-varManuf=1+CVm.*randn(1,n);
+  while sum(abs(h-hant))>1e-3;
+    hant=h;
+    q=(k.*h.^x).*varManuf;
+    h=h0.*vectorUnos-zR-transpose(matrizAcum)*(0.465.*(matrizAcum*q).^1.75.*D.^-4.75.*(le+s));
+  end
 
-h=h0.*vectorUnos;
-hant=0.*vectorUnos;
-
-i=0;
-tic;
-while sum(abs(h-hant))>1e-6;
-  hant=h;
-  q=(k.*h.^x).*varManuf;
-  ++i;
-  h=h0.*vectorUnos-zR-(0.465.*(q*tril(ones(n),0)).^1.75.*D.^-4.75.*(le+s))*transpose(matrizAcum);
 end
-toc
-
-%Resultados
-i
-h0
-CVq=std(q)/mean(q)
-CVqh=std(k.*h.^x)/mean(k.*h.^x)
-CVqm=sqrt(CVq^2-CVqh^2)
-
-plot(xR,zR,"-;z(x);");hold on;plot(xR,h,"-;h(x);");plot(xR,-q,"o;q(x);");hold off;
--- unidad.m
+++ unidad.m
@@ -1,10 +1,42 @@
-nr=10;ng=20;h0=10;k=1.25;x=0.54;CVm=0.05;le=0.5;s=0.5;D=13.6;I0=0.05;
+nR=25;ng=100;h0=10;k=1.25;x=0.54;CVm=0.05;leg=0.75;sr=0.5;Dr=13.6;I0r=0.02;lepR=1.5;spR=1;DpR=25;I0pR=0;
 
-h=h0.*ones(ng,nr);
-q=k.*h.^x;
+%Se calcula la distribución de presión hpR en la tubería portarramal
+tic;
 
-%Caudal en los tramos del ramal
-Qr=triu(ones(ng),0)*q
+vectorUnos=ones(1,nR);
+matrizAcum=triu(ones(nR),0);
 
-%Caudal en los tramos de la tubeŕia portarramal
-Qpr=(ones(1,ng)*q)*tril(ones(nr),0)
+xpR=(spR.*vectorUnos)*matrizAcum;
+zpR=-I0pR.*xpR;
+
+hpR=h0.*vectorUnos;
+hpRant=0.*vectorUnos;
+
+%Variación manufactura
+varManuf=1+CVm.*randn(ng,nR);
+
+cont=0;
+
+while sum(abs(hpR-hpRant))>1e-3;
+  hpRant=hpR;
+  for j=nR:-1:1
+    %Presiones y caudales en los goteros del ramal j
+    [h,q]=ramal(hpRant(j),ng,k,x,CVm,leg,sr,Dr,I0r,varManuf(:,j));
+    hr(:,nR+1-j)=h;%Matriz de presiones en los goteros de la unidad
+    qr(:,nR+1-j)=q;%Matriz de caudales de los goteros de la unidad
+  end
+
+  %Caudal en los tramos de la tubeŕia portarramal
+  QpR=(ones(1,ng)*qr)*transpose(matrizAcum);
+  hpR=h0.*vectorUnos-zpR-(0.465.*QpR.^1.75.*DpR.^-4.75.*(lepR+spR));
+  ++cont;
+end
+
+toc
+
+qh=(k.*hr.^x);
+
+CVq=std2(qr)/mean2(qr)
+CVqh=std2(qh)/mean2(qh)
+CVm=sqrt(CVq^2-CVqh^2)
+
